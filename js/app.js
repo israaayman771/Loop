0ee97f76da122d3406ef035fd791571bf2fe0bae
israaayman <israaayman771>
2024-03-13 17:43:31 +0200

    first commit

---
+++ js/app.js
@@ -0,0 +1,14 @@
+//1
+var studentScore =[10 ,20 ,30 ,40]
+console.log(studentScore);
+
+var index = 0;
+while (index <=3){
+    console.log(studentScore[index]);
+    studentScore[index] = studentScore[index]+5;
+    index = index+1
+
+}
+
+//2
+var studentScore = [5 ,15 ,20]
